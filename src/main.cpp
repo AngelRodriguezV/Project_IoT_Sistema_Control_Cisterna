321c4f09bf5d06a5ec33c961aa7d0732d414f5ce
Rodríguez Venegas Ángel de Jesús <43628553+AngelRodriguezV@users.noreply.github.com>
2023-11-27 18:32:43 -0600

    code v1.0

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,42 @@
 
 #define SOUND_VELOCITY 0.034
 
-long duration;
-float distanceCm;
+long durationS1;
+float distanceCmS1;
+long durationS2;
+float distanceCmS2;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(D1, OUTPUT);
-  pinMode(D0, INPUT);
+  pinMode(D0, OUTPUT); // Trig D0
+  pinMode(D1, INPUT); // Echo D1
+  pinMode(D5, OUTPUT); // Trig D5
+  pinMode(D6, INPUT); // Echo D6
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(D1, LOW);
+  digitalWrite(D0, LOW); 
+  digitalWrite(D5, LOW); 
   delayMicroseconds(2);
 
-  digitalWrite(D1, HIGH);
+  digitalWrite(D0, HIGH);
+  digitalWrite(D5, HIGH);
   delayMicroseconds(10);
-  digitalWrite(D1, LOW);
+  digitalWrite(D0, LOW);
+  digitalWrite(D5, LOW);
 
-  duration = pulseIn(D0, HIGH);
+  durationS1 = pulseIn(D1, HIGH);
+  durationS2 = pulseIn(D6, HIGH);
 
-  distanceCm = duration * SOUND_VELOCITY / 2;
+  distanceCmS1 = durationS1 * SOUND_VELOCITY / 2;
+  distanceCmS2 = durationS2 * SOUND_VELOCITY / 2;
 
-  Serial.println("Distance (cm): ");
-  Serial.println(distanceCm);
+  Serial.println("DistanceS1 (cm): ");
+  Serial.println(distanceCmS1);
+  Serial.println("DistanceS2 (cm): ");
+  Serial.println(distanceCmS2);
 
   delay(1000);
 }
