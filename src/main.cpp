9acfb327a7cae9f2b208ecfd00935dc0943686f4
Rodríguez Venegas Ángel de Jesús <43628553+AngelRodriguezV@users.noreply.github.com>
2023-11-28 00:30:46 -0600

    code error - Codigo para el recurso

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,33 @@
 #include <Arduino.h>
+#include <NewPing.h>
 
 #define SOUND_VELOCITY 0.034
 
-long durationS1;
-float distanceCmS1;
-long durationS2;
-float distanceCmS2;
+#define TRIGGER_PIN_1 D1
+#define ECHO_PIN_1 D2
+#define TRIGGER_PIN_2 D3
+#define ECHO_PIN_2 D4
+
+NewPing sonar1(TRIGGER_PIN_1, ECHO_PIN_1, 200);
+NewPing sonar2(TRIGGER_PIN_2, ECHO_PIN_2, 200);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(D0, OUTPUT); // Trig D0
-  pinMode(D1, INPUT); // Echo D1
-  pinMode(D5, OUTPUT); // Trig D5
-  pinMode(D6, INPUT); // Echo D6
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(D0, LOW); 
-  digitalWrite(D5, LOW); 
-  delayMicroseconds(2);
+  delay(50);
 
-  digitalWrite(D0, HIGH);
-  digitalWrite(D5, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(D0, LOW);
-  digitalWrite(D5, LOW);
+  unsigned int distance1 = sonar1.ping_cm();
+  unsigned int distance2 = sonar2.ping_cm();
 
-  durationS1 = pulseIn(D1, HIGH);
-  durationS2 = pulseIn(D6, HIGH);
+  Serial.print("Distance Sensor 1: ");
+  Serial.print(distance1);
+  Serial.print(" cm\t");
 
-  distanceCmS1 = durationS1 * SOUND_VELOCITY / 2;
-  distanceCmS2 = durationS2 * SOUND_VELOCITY / 2;
-
-  Serial.println("DistanceS1 (cm): ");
-  Serial.println(distanceCmS1);
-  Serial.println("DistanceS2 (cm): ");
-  Serial.println(distanceCmS2);
-
-  delay(1000);
+  Serial.print("Distance Sensor 2: ");
+  Serial.print(distance2);
+  Serial.println(" cm");
 }
